87be96a1955d8d696a93eabd2ba9cdd601aa85dc
Lursmani <lursmani9@gmail.com>
2021-02-19 22:15:31 +0400

    carousel 1

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,13 +1,66 @@
 import React from "react"
+import "./projects.css"
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import MainProjectCard from "./mainprojectcard"
+
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 1
+    }
+  
+}
 
 
 function Projects(props) {
 
     return (
-        <div>
-            <h1>projects</h1>
+        <div className="projects-container">
+            <div className="projects-title-div">
+                <h1 className="projects-title">My Projects:</h1>
+            </div>
+            <ProjectCarousel 
+            title="FreeCodeCamp projects"
+            description="These are the first projects I did while I was learning HTML and CSS">
+                <MainProjectCard />
+                <MainProjectCard />
+                <MainProjectCard />
+                <MainProjectCard />
+
+            </ProjectCarousel>
+            
+           
         </div>
     )
 }
+
+
+function ProjectCarousel(props) {
+
+    return (
+        <div className="carousel-div">
+             <h1 className="carousel-title">{props.title}</h1>
+             <p className="carousel-description">{props.description}</p>
+             <Carousel responsive={responsive}>
+                {props.children}
+            </Carousel>
+        </div>
+    )
+}
+
+
+
 
 export default Projects
---
+++ src/components/projects/mainprojectcard.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react"
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import "./mainprojectcard.css"
+
+AOS.init(
+
+    );
+
+function MainProjectCard(props) {
+    const [hover, setHovered] = useState(false)
+    
+    function handleMouseOver() {
+        setHovered(true) 
+    }
+    function handleMouseLeave() {
+        setHovered(false)
+    }
+
+    const buttonClass = hover ? "link-button-hover" : "link-button-nohover"
+    const overlayClass = hover ? "project-description-div-in" : "project-description-div-out" //this is supposed to animate on hoevr-out, but doesn't
+
+    return (
+            <div className="project-card-div" onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave}>
+                <img src={props.image} alt={props.imageDescription} className="project-img" />
+                <a href={props.destination} className="project-link">
+                    <button className={buttonClass} >{props.linkText}</button>
+                </a>
+
+                {/* This activates the overlay on hover. 
+                 I want to make this part disappear after hoevrv-out animation is done. Will look up later */}
+                {hover && (
+                <div className={overlayClass}   >
+                    <p className="project-description" data-aos="fade-up" data-aos-delay="200" >
+                        {props.description} 
+                    </p>
+                    <div className="project-overlay"  ></div> 
+                </div> )}
+                
+             
+                
+
+            </div>
+    )
+}
+
+export default MainProjectCard
